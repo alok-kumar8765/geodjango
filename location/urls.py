af84b5afc9b5940dc4c27dbcf8eeb153cbba8b12
alok-techqware <alok.kumarlove20@gmail.com>
2022-02-04 20:23:35 +0530

    Update urls.py

--- location/urls.py
+++ location/urls.py
@@ -1,7 +1,14 @@
 from re import I
-from django.urls import path
+from django.urls import path,include
 from .views import *#HotelUpdateRetreiveView, ListCreateGenericViews, addpoint, allpoints,index, viewpoints,HotelCreateAPIView,ListCreateGenericAPIViews,DetaileHotelApiView
 from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'nairobihealthfacilities', views.NairobiHealthFacilitiesViewSet)
+router.register(r'nairobisubcounties', views.NairobiSubCountiesViewSet)
+
 urlpatterns = [
     path("hotels/", ListCreateGenericViews.as_view()),
     path("hotels/<str:pk>/",HotelUpdateRetreiveView.as_view()),
@@ -12,6 +19,6 @@ urlpatterns = [
     path('addlocation/',addpoint,name='addpoint'),
     path('viewpoints/',viewpoints,name='viewpoints'),
     path('allpoints/',allpoints,name='allpoints'),
-    path('hosptal/',views.NairobiSubCountiesViewSet),
-    path('hospital2/',views.NairobiHealthFacilitiesViewSet),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
