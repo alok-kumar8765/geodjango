a2797770a3e00eeeabc8d79fac4d5e01bd87a06d
alok-techqware <alok.kumarlove20@gmail.com>
2022-02-04 16:55:59 +0530

    d
    d

--- location/views.py
+++ location/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Hotel,Location,HotelTwo
+from .models import Hotel,Location,HotelTwoT
 from rest_framework.generics import GenericAPIView
 from django.contrib.gis.geos import Point
 from geopy.geocoders import Nominatim
